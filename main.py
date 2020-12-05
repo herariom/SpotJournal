825f1f9455a02b5d86939c549dca87f87584e4b5
Logan Johnson <logan.johnson189@gmail.com>
2020-12-05 14:59:40 -0500

    Created results template

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import os
 app = Flask(__name__)
 
 
+@app.route('/results')
+def results():
+    legend = "Results"
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+
+    return render_template('results.html', values=values, labels=labels, legend=legend)
+
 @app.route('/')
 def index():
     return render_template('index.html')
