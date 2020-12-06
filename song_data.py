9af26a4b305853cc874570f85c3b7ce5c6d7e333
Logan Johnson <logan.johnson189@gmail.com>
2020-12-05 20:13:42 -0500

    Removed dataclasses because Google Cloud in Python 3.6

--- song_data.py
+++ song_data.py
@@ -1,8 +1,12 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class SongData():
+
+    def __init__(self, legend, values, color_r, color_g, color_b):
+        self.legend = legend
+        self.values = values
+        self.color_r = color_r
+        self.color_g = color_g
+        self.color_b = color_b
+
     legend: str
     values: list
     color_r: int
